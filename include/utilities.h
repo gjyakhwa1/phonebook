69d0b7ee08517d7744a55aed161ce884c62a2cfc
gjyakhwa1 <gjyakhwa1@gmail.com>
2020-05-17 21:34:04 +0545

    Updated all

---
+++ include/utilities.h
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <iostream>
+#include <regex>
+using namespace std;
+bool email_isvalid(char* email);
+bool phoneNumber_isvalid(char* phone_number);
+const int MAX_ENTRY=100;
+
+
+#endif // UTILITIES_H
